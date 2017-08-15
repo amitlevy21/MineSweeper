fd2bf490d08e22a6b1fb16358353e3ae8e3fd50b
ravidanb <30519470+ravidanb@users.noreply.github.com>
2017-08-15 22:17:15 +0300

    buttons in play and endgame activities work, endfame show "you lost" if quit button pressed in play, main activity show last played sificullty

--- app/src/main/java/com/example/amit/minesweeper/EndGameActivity.java
+++ app/src/main/java/com/example/amit/minesweeper/EndGameActivity.java
@@ -1,7 +1,11 @@
 package com.example.amit.minesweeper;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class EndGameActivity extends AppCompatActivity {
 
@@ -9,5 +13,27 @@ public class EndGameActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_end_game);
+
+        Bundle bundle = getIntent().getExtras();
+        boolean result = (boolean) bundle.getSerializable("result");
+        Button tryAgain = (Button) findViewById(R.id.try_again);
+
+        if (result) {
+            TextView textResult = (TextView) findViewById(R.id.result);
+            textResult.setText(getString(R.string.result) + " you have won");
+        } else {
+            TextView textResult = (TextView) findViewById(R.id.result);
+            textResult.setText(getString(R.string.result) + " you have lost");
+        }
+
+        tryAgain.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Intent intent = new Intent(view.getContext(), MainActivity.class);
+                startActivity(intent);
+
+            }
+        });
     }
 }
