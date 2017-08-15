034e72970f1aa7a8664aef8779059c0e28d23b1a
amitlevy21 <amitlevy347@gmail.com>
2017-08-15 22:25:05 +0300

    buttons in play and endgame activities work, endfame show "you lost" if quit button pressed in play, main activity show last played sificullty

--- app/src/main/java/com/example/amit/minesweeper/EndGameActivity.java
+++ app/src/main/java/com/example/amit/minesweeper/EndGameActivity.java
@@ -33,7 +33,9 @@ public class EndGameActivity extends AppCompatActivity {
                 Intent intent = new Intent(view.getContext(), MainActivity.class);
                 startActivity(intent);
 
+
             }
         });
     }
+    //test
 }
